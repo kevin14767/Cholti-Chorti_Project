35c3a0d1c6bfc06400b8baff596a1b3927af04e3
Kevin Barcenas <95306723+kevin14767@users.noreply.github.com>
2023-11-01 11:15:16 -0500

    Update removeBlankLines.py

--- pythonScripts/removeBlankLines.py
+++ pythonScripts/removeBlankLines.py
@@ -1,12 +1,5 @@
-#this python script is intended to convert "The Religious Sections" of the Moran Manuscript into a database file
-#Excel sheet format will be a answer and response format, naturally the manuscript follows these format as well
-
-#Formatting:
-#Line Number 1-530
-#Manuscript entry Page.59 to 80
-#Line entry in the actual manuscript page. i.e entry number 1 entry number 2 however sometimes they are in the same line entry or in
-#multiple lines for one entry. 
-#4 columns: standardized version of the original ch'olti, morphemic gloss, a literal english translation, a flowing english translation
+#python script just removed any empty lines in the Original Transcription.txt file to make the extraction of data easier for the txtFileToExcel.py script
+#writes output to OriginalTranscriptEdited.txt
 
 import pandas as pd
 
